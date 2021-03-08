d5756ee8072a51a0b0fd385f5fd98aee11a4fb6b
KyungMin <2004ckck@naver.com>
2021-03-08 23:41:49 +0900

    [CREATE] room Sign_Up MVVM 패턴중 Repository 만듬

---
+++ app/src/main/java/com/example/it_consultiong/data/repository/SignUpRepository.kt
@@ -0,0 +1,17 @@
+package com.example.it_consultiong.data.repository
+
+import androidx.lifecycle.LiveData
+import com.example.it_consultiong.Dao.SignUpDao
+import com.example.it_consultiong.data.models.SignUpData
+
+
+class SignUpRepository(private val signUpDao: SignUpDao) {
+    val getAllData: LiveData<List<SignUpData>> = signUpDao.getAllData()
+    val getSignUpData: LiveData<List<SignUpData>> = signUpDao.getAllData()
+
+    suspend fun insertData(signUpData: SignUpData) {
+        signUpDao.insertData(signUpData)
+    }
+
+
+}
