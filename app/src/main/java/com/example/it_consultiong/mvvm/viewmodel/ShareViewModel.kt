9d82dcad93508dd1e85cd7e91e0bfac73d26a36b
KyungMin <2004ckck@naver.com>
2021-03-15 19:18:11 +0900

    [Update] Null check 함수 추가

--- app/src/main/java/com/example/it_consultiong/mvvm/viewmodel/ShareViewModel.kt
+++ app/src/main/java/com/example/it_consultiong/mvvm/viewmodel/ShareViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import com.example.it_consultiong.mvvm.models.SignUpData
 
+@Suppress("UNREACHABLE_CODE")
 class ShareViewModel(application: Application) : AndroidViewModel(application) {
 
     val emptyDatabase: MutableLiveData<Boolean> = MutableLiveData(true)
@@ -16,29 +17,36 @@ class ShareViewModel(application: Application) : AndroidViewModel(application) {
     }
 
     fun verifyDtaFromUser(
-            name: String,
-            id: String,
-            password: String,
-            grade: String,
-            signCalss: String,
-            classNumber: String,
-            ): Boolean {
+        name: String,
+        id: String,
+        password: String,
+        grade: String,
+        signCalss: String,
+        classNumber: String,
+        major:String
+    ): Boolean {
         // TextUtils 이란, Android 에서 제공하는 TextView null 체크 방식이다.
         return if (TextUtils.isEmpty(name) || TextUtils.isEmpty(id) || TextUtils.isEmpty(password) || TextUtils.isEmpty(
-                        grade
-                ) || TextUtils.isEmpty(signCalss) || TextUtils.isEmpty(classNumber)
+                grade
+            ) || TextUtils.isEmpty(signCalss) || TextUtils.isEmpty(classNumber) ||TextUtils.isEmpty(major)
         ) {
             false
         } else
             !(TextUtils.isEmpty(name) || TextUtils.isEmpty(id) || TextUtils.isEmpty(password) || TextUtils.isEmpty(
-                    grade
-            ) || TextUtils.isEmpty(signCalss) || TextUtils.isEmpty(classNumber))
+                grade
+            ) || TextUtils.isEmpty(signCalss) || TextUtils.isEmpty(classNumber))||TextUtils.isEmpty(major)
 
     }
 
+    fun signNullTest(id: String, pwd: String): Boolean {
+        return if (TextUtils.isEmpty(id) || TextUtils.isEmpty(pwd))
+            return false
+        else {
+            !(TextUtils.isEmpty(id) || TextUtils.isEmpty(pwd))
 
+        }
 
-
+    }
 
 
 }
