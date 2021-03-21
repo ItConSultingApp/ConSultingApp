a0ddf8c6f02a6b4ec4d82e1670b9ff15725a508f
KyungMin <2004ckck@naver.com>
2021-03-21 16:52:39 +0900

    [add] major 추가

--- app/src/main/java/com/cotion/it_consultiong/data/data_model/Major.kt
+++ app/src/main/java/com/cotion/it_consultiong/data/data_model/Major.kt
@@ -1,7 +1,52 @@
 package com.cotion.it_consultiong.data.data_model
 
-import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 
-data class Major (val and_img:ImageView, val and_text: TextView, val img_etc:ImageView, val etc:TextView)
+data class Major(
+    val and_img: ImageView?,
+    val and_text: TextView?,
+
+    val ios_img:ImageView?,
+    val ios_text:TextView?,
+
+    val web_front_img: ImageView?,
+    val web_front_text: TextView?,
+
+    val web_back_img: ImageView?,
+    val web_back_text: TextView?,
+
+    val web_function_img: ImageView?,
+    val web_function_text: TextView?,
+
+    val game_img: ImageView?,
+    val game_text: TextView?,
+
+    val game_function_img: ImageView?,
+    val game_function_text: TextView?,
+
+    val Cyber_security_img: ImageView?,
+    val Cyber_security_text: TextView?,
+
+    val Cyber_security_function_img: ImageView?,
+    val Cyber_security_function_text: TextView?,
+
+    val ai_img: ImageView?,
+    val ai_text:TextView?,
+
+    val threeD_img:ImageView?,
+    val threeD_text:TextView?,
+
+    val cloud_img:ImageView?,
+    val cloud_text:TextView?,
+
+    val iot_img:ImageView?,
+    val iot_text:TextView?,
+
+    val robotics_img:ImageView?,
+    val robotics_text:TextView?,
+
+    val etc_img: ImageView?,
+    val etc_text: TextView?,
+
+    )
