a5631f1b47c0ed8bb37bafe41c8800c5aff2d2be
KyungMin <2004ckck@naver.com>
2021-03-26 11:35:59 +0900

    error commit

---
+++ app/src/main/java/com/cotion/it_consultiong/Model/Cafeteria_model.kt
@@ -0,0 +1,18 @@
+package com.example.school_cafeteria.Model
+
+data class Cafeteria_model(
+    val meal: MealServiceDietInfo,
+    val head: Head,
+    val result: RESULT,
+    val row: Row
+)
+
+
+
+
+
+
+
+
+
+
---
+++ app/src/main/java/com/cotion/it_consultiong/Model/Row.kt
@@ -0,0 +1,7 @@
+package com.example.school_cafeteria.Model
+
+data class Row(
+    val DDISH_NM: String,
+    val MMEAL_SC_NM: String,
+
+)
