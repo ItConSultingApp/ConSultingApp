adcbe7945dd912afeb2887aebed0a06d264c9639
KyungMin <2004ckck@naver.com>
2021-03-27 13:24:08 +0900

    [Create] Meal(급식)

---
+++ app/src/main/java/com/cotion/it_consultiong/network/RetrofitBuilder.kt
@@ -0,0 +1,41 @@
+package com.example.school_cafeteria.Ui
+
+import com.example.school_cafeteria.RetrofitService
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+
+object RetrofitBuilder {
+
+
+    // 서버 주소
+    private const val BASE_URL = "https://open.neis.go.kr/"
+
+
+    var service: RetrofitService
+
+
+    init {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(createOkHttpClient())
+            .build()
+
+
+        service = retrofit.create(RetrofitService::class.java)
+    }
+
+    private fun createOkHttpClient(): OkHttpClient {
+        val builder = OkHttpClient.Builder()
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.level = HttpLoggingInterceptor.Level.BODY
+        builder.addInterceptor(interceptor)
+        return builder.build()
+    }
+}
+
+
