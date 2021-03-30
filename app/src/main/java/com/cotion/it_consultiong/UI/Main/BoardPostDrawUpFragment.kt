ce0b81ccdc6b44201d3d4e07e4ecfbda2d56f46f
KyungMin <2004ckck@naver.com>
2021-03-30 20:24:40 +0900

    [Update] 파이어베이스 형식 변경

--- app/src/main/java/com/cotion/it_consultiong/UI/Main/BoardPostDrawUpFragment.kt
+++ app/src/main/java/com/cotion/it_consultiong/UI/Main/BoardPostDrawUpFragment.kt
@@ -1,21 +1,23 @@
 package com.cotion.it_consultiong.UI.Main
 
-import androidx.fragment.app.Fragment
 import android.os.Bundle
 import android.text.TextUtils
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
-
 import com.cotion.it_consultiong.R
-import com.cotion.it_consultiong.UI.FragmentMaInActivity
-import com.cotion.it_consultiong.data.data_model.signInUserInfo
+import com.cotion.it_consultiong.UI.Main.BoardFragment.Companion.TAG
 import com.cotion.it_consultiong.databinding.FragmentBoardPostDrawUpBinding
+import com.cotion.it_consultiong.model.recycler_model.BoardData
+import com.cotion.it_consultiong.mvvm.viewmodel.MealViewModel
 import com.cotion.it_consultiong.mvvm.viewmodel.ObjectClass
+import com.cotion.it_consultiong.mvvm.viewmodel.ShareViewModel
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
-import java.text.SimpleDateFormat
 import java.util.*
 
 @Suppress("RECEIVER_NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
@@ -24,13 +26,17 @@ class BoardPostDrawUpFragment : Fragment() {
     private val binding get() = _binding!!
     private val db = FirebaseFirestore.getInstance()
     private val auth = FirebaseAuth.getInstance()
+    private val mealViewModel: MealViewModel by viewModels()
+
+    private val shareViewModel: ShareViewModel by viewModels()
 
     //나중에 앞쪽에서 전역변수등으로 선언하기
     val objectClass = ObjectClass()
 
     private val uid = auth.currentUser?.uid // null
     private val currentDateTime = Calendar.getInstance().time
-    private lateinit var dateFormat: String
+
+    private lateinit var userInfo: BoardData
     private lateinit var postInfo: HashMap<String, String?>
 
     override fun onCreateView(
@@ -46,34 +52,44 @@ class BoardPostDrawUpFragment : Fragment() {
                 postInfo()
             } else objectClass.showToast(context, "내용을 입력해주세요")
         }
-        return binding.root
 
+        userInfo = BoardData(
+            binding.postName.text.toString(),
+            "",
+            binding.postTxt.text.toString(),
+            "",
+            mealViewModel.formatted_custon
+        )
+
+
+        return binding.root
 
     }
 
 
     private fun postInfo() {
-        val userInfo = signInUserInfo()
-        dateFormat = SimpleDateFormat("yyyy.MM.dd HH:mm:ss", Locale.KOREA).format(currentDateTime)
+
+
         postInfo = hashMapOf(
             "name" to userInfo.name,
-            "grade" to userInfo.grade,
-            "grad_class" to userInfo.grad_class,
-            "class_number" to userInfo.class_number,
-            "job" to userInfo.job,
-            "profile" to userInfo.profileImg,
-            "when" to dateFormat,
-            "contents" to binding.postTxt.text.toString()
+            "title" to userInfo.title,
+            "context" to binding.postTxt.text.toString(),
+            "profile" to userInfo.profile,
+            "day" to userInfo.day,
         )
 
-        db.collection("board_post").document(uid + dateFormat)
+        db.collection("board_post").document(uid + mealViewModel.formatted_custon)
             .set(postInfo)
             .addOnSuccessListener {
                 objectClass.showToast(requireContext(), "게시글 올리기 성공")
                 findNavController().navigate(R.id.action_fragment_board_post_navi_to_fragment_board_navi)
+                Log.d(TAG, "postInfo: ${userInfo.context}")
+                Log.d(TAG, "postInfo: $postInfo")
             }
             .addOnFailureListener {
                 objectClass.showToast(requireContext(), "게시글 올리기 실패")
             }
     }
+
+
 }
