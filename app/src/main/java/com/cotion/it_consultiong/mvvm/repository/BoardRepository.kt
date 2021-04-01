50da03b0e4f301ba235483ada05031ab9c8e15d1
KyungMin <2004ckck@naver.com>
2021-04-01 14:19:58 +0900

    [Delete] 일시적인 제거

---
+++ app/src/main/java/com/cotion/it_consultiong/mvvm/repository/BoardRepository.kt
@@ -0,0 +1,10 @@
+package com.cotion.it_consultiong.mvvm.repository
+
+import androidx.lifecycle.LiveData
+import com.cotion.it_consultiong.Dao.SignUpDao
+import com.cotion.it_consultiong.model.recycler_model.BoardData
+import com.cotion.it_consultiong.mvvm.models.SignUpData
+
+
+class BoardRepository(private val boardData: BoardData) {
+}
