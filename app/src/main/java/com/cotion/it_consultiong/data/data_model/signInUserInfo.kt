0ba194fb03ff19a6347548ce7679fcae2bf7270d
KyungMin <2004ckck@naver.com>
2021-04-01 14:23:10 +0900

    init commit

--- app/src/main/java/com/cotion/it_consultiong/data/data_model/signInUserInfo.kt
+++ app/src/main/java/com/cotion/it_consultiong/data/data_model/signInUserInfo.kt
@@ -2,13 +2,13 @@ package com.cotion.it_consultiong.data.data_model
 
 data class signInUserInfo(
     val name: String?,
-    val grade:String?,
-    val grad_class : String?,
-    val class_number:String?,
-    val job : String?,
-    val profileImg : String?
-){
-    constructor(): this(null,null,null,null,null,null){
+    val grade: String?,
+    val grad_class: String?,
+    val class_number: String?,
+    val job: String?,
+    val profileImg: String?
+) {
+    constructor() : this(null, null, null, null, null, null) {
 
     }
 
