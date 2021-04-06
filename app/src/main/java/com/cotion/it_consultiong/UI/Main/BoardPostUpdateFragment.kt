76463aca24ecfe285d6ed68db8c371b03b793088
KyungMin <2004ckck@naver.com>
2021-04-06 19:42:19 +0900

    [CREATE] 게시판 Update 창 아직 미완성

---
+++ app/src/main/java/com/cotion/it_consultiong/UI/Main/BoardPostUpdateFragment.kt
@@ -0,0 +1,89 @@
+package com.cotion.it_consultiong.UI.Main
+
+import android.R
+import android.content.Intent
+import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Spinner
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.cotion.it_consultiong.UI.FragmentMainActivity
+import com.cotion.it_consultiong.UI.Main.HomeFragment.Companion.TAG
+import com.cotion.it_consultiong.UI.Main.Splash.Companion.userJob
+import com.cotion.it_consultiong.UI.Main.Splash.Companion.userName
+import com.cotion.it_consultiong.databinding.FragmentBoardPostDrawUpBinding
+import com.cotion.it_consultiong.databinding.FragmentUpdateBoardBinding
+import com.cotion.it_consultiong.model.recycler_model.BoardData
+import com.cotion.it_consultiong.mvvm.viewmodel.ObjectClass
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import java.text.SimpleDateFormat
+import java.util.*
+
+
+@Suppress("RECEIVER_NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
+class BoardPostUpdateFragment : Fragment() {
+    private var _binding: FragmentUpdateBoardBinding? = null
+    private val binding get() = _binding!!
+    private val db = FirebaseFirestore.getInstance()
+    private val auth = FirebaseAuth.getInstance()
+
+    //나중에 앞쪽에서 전역변수등으로 선언하기
+    val objectClass = ObjectClass()
+    val bundle = Bundle()
+    private val uid = auth.currentUser?.uid // null
+    private val currentDateTime = Calendar.getInstance().time
+    private lateinit var dateFormat: String
+
+    private lateinit var boardData: BoardData
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentUpdateBoardBinding.inflate(inflater, container, false)
+
+
+        binding.postCurrentUpload.setOnClickListener {
+            if (!TextUtils.isEmpty(binding.postCurrentTxt.text.toString().trim { it <= ' ' })) {
+                postInfo()
+            } else objectClass.showToast(requireContext(), "내용을 입력해주세요")
+        }
+        return binding.root
+
+
+    }
+
+
+    private fun postInfo() {
+        dateFormat = SimpleDateFormat("yyyy.MM.dd HH:mm:ss", Locale.KOREA).format(currentDateTime)
+
+        boardData = BoardData(
+            binding.postCurrentTxt.text.toString(),
+            userJob.toString(),
+            userName,
+            "",
+            dateFormat
+        )
+
+
+        db.collection("board_post").document(auth.currentUser.uid + dateFormat)
+
+            .update("board_post", boardData)
+
+            .addOnSuccessListener {
+                objectClass.showToast(requireContext(), "게시글 올리기 성공")
+                Log.d(TAG, "postInfo: $boardData ")
+
+                findNavController().navigate(com.cotion.it_consultiong.R.id.action_fragment_board_post_navi_to_fragment_board_navi)
+            }
+            .addOnFailureListener {
+                objectClass.showToast(requireContext(), "게시글 올리기 실패")
+            }
+    }
+}
