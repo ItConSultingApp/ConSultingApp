597bb9f887496beb14966d1ca7f40102c13a0dea
KyungMin <2004ckck@naver.com>
2021-04-06 19:44:58 +0900

    [DELETE] PULL하면서 생긴 HEAD 삭제

--- app/src/main/java/com/cotion/it_consultiong/UI/FragmentMainActivity.kt
+++ app/src/main/java/com/cotion/it_consultiong/UI/FragmentMainActivity.kt
@@ -1,28 +1,20 @@
 package com.cotion.it_consultiong.UI
 
+
 import android.os.Bundle
-<<<<<<< HEAD
 import android.view.View
-=======
-import android.util.Log
->>>>>>> aed244012055c48ca335f17593cd8cade4cb41d8
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation.findNavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.cotion.it_consultiong.R
-<<<<<<< HEAD
-=======
-import com.cotion.it_consultiong.data.data_model.signInUserInfo
->>>>>>> aed244012055c48ca335f17593cd8cade4cb41d8
 import com.cotion.it_consultiong.databinding.FragmentMainBinding
-
-
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
-class FragmentMaInActivity : AppCompatActivity() {
+class FragmentMainActivity : AppCompatActivity() {
 
     private val TAG = "FragmentMaInActivity"
     private val binding by lazy { FragmentMainBinding.inflate(layoutInflater) }
@@ -32,10 +24,6 @@ class FragmentMaInActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-<<<<<<< HEAD
-=======
-
->>>>>>> aed244012055c48ca335f17593cd8cade4cb41d8
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
         setSupportActionBar(binding.toolbar)
@@ -48,36 +36,28 @@ class FragmentMaInActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
-<<<<<<< HEAD
         initNavigation()
 
     }
 
-=======
 
-
-    }
->>>>>>> aed244012055c48ca335f17593cd8cade4cb41d8
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment)
         return navController.navigateUp() || super.onSupportNavigateUp()
     }
-<<<<<<< HEAD
 
     private fun initNavigation() {
         val navController = findNavController(R.id.nav_host_fragment)
         binding.navView.setupWithNavController(navController)
 
         navController.addOnDestinationChangedListener { _, destination, _ ->
-            if (destination.id == R.id.fragment_board_navi || destination.id == R.id.fragment_profile_navi || destination.id == R.id.fragment_home_navi|| destination.id == R.id.fragment_home_navi) {
+            if (destination.id == R.id.fragment_board_navi || destination.id == R.id.fragment_profile_navi || destination.id == R.id.fragment_home_navi || destination.id == R.id.fragment_home_navi) {
                 binding.navView.visibility = View.VISIBLE
             } else {
                 binding.navView.visibility = View.GONE
             }
         }
     }
-=======
->>>>>>> aed244012055c48ca335f17593cd8cade4cb41d8
 }
 
 
