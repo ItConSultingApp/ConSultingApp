7eb8687eb2acb50293e14310cd09c0b25cc43071
KyungMin <2004ckck@naver.com>
2021-04-06 19:41:22 +0900

    [UPDATE] 파이어베이스 에 들어갈 변수를 HashMAP->  DataClass 로 변경

--- app/src/main/java/com/cotion/it_consultiong/UI/Main/BoardPostDrawUpFragment.kt
+++ app/src/main/java/com/cotion/it_consultiong/UI/Main/BoardPostDrawUpFragment.kt
@@ -1,28 +1,28 @@
 package com.cotion.it_consultiong.UI.Main
 
-import androidx.fragment.app.Fragment
+import android.R
+import android.content.Intent
 import android.os.Bundle
 import android.text.TextUtils
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Spinner
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-
-import com.cotion.it_consultiong.R
-import com.cotion.it_consultiong.UI.FragmentMaInActivity
-import com.cotion.it_consultiong.UI.Main.Splash.Companion.userClass
-import com.cotion.it_consultiong.UI.Main.Splash.Companion.userGrade
+import com.cotion.it_consultiong.UI.FragmentMainActivity
+import com.cotion.it_consultiong.UI.Main.HomeFragment.Companion.TAG
 import com.cotion.it_consultiong.UI.Main.Splash.Companion.userJob
 import com.cotion.it_consultiong.UI.Main.Splash.Companion.userName
-import com.cotion.it_consultiong.UI.Main.Splash.Companion.userNumber
-import com.cotion.it_consultiong.UI.Main.Splash.Companion.userProfile
-import com.cotion.it_consultiong.data.data_model.signInUserInfo
 import com.cotion.it_consultiong.databinding.FragmentBoardPostDrawUpBinding
+import com.cotion.it_consultiong.model.recycler_model.BoardData
 import com.cotion.it_consultiong.mvvm.viewmodel.ObjectClass
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import java.text.SimpleDateFormat
 import java.util.*
+
 
 @Suppress("RECEIVER_NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
 class BoardPostDrawUpFragment : Fragment() {
@@ -33,16 +33,17 @@ class BoardPostDrawUpFragment : Fragment() {
 
     //나중에 앞쪽에서 전역변수등으로 선언하기
     val objectClass = ObjectClass()
-
+    val bundle = Bundle()
     private val uid = auth.currentUser?.uid // null
     private val currentDateTime = Calendar.getInstance().time
     private lateinit var dateFormat: String
-    private lateinit var postInfo: HashMap<String, String?>
+
+    private lateinit var boardData: BoardData
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
 
         _binding = FragmentBoardPostDrawUpBinding.inflate(inflater, container, false)
 
@@ -60,22 +61,23 @@ class BoardPostDrawUpFragment : Fragment() {
 
     private fun postInfo() {
         dateFormat = SimpleDateFormat("yyyy.MM.dd HH:mm:ss", Locale.KOREA).format(currentDateTime)
-        postInfo = hashMapOf(
-            "name" to userName,
-            "grade" to userGrade,
-            "grad_class" to userClass,
-            "class_number" to userNumber,
-            "job" to userJob,
-            "profile" to userProfile.toString(),
-            "when" to dateFormat,
-            "contents" to binding.postTxt.text.toString()
+
+        boardData = BoardData(
+            binding.postTxt.text.toString(),
+            userJob.toString(),
+            userName,
+            "",
+            dateFormat
         )
 
-        db.collection("board_post").document(uid + dateFormat)
-            .set(postInfo)
+
+        db.collection("board_post").document(auth.currentUser.uid + dateFormat)
+            .set(boardData)
             .addOnSuccessListener {
                 objectClass.showToast(requireContext(), "게시글 올리기 성공")
-                findNavController().navigate(R.id.action_fragment_board_post_navi_to_fragment_board_navi)
+                Log.d(TAG, "postInfo: $boardData ")
+
+                findNavController().navigate(com.cotion.it_consultiong.R.id.action_fragment_board_post_navi_to_fragment_board_navi)
             }
             .addOnFailureListener {
                 objectClass.showToast(requireContext(), "게시글 올리기 실패")
