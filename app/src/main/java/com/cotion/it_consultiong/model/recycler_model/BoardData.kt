bfcdc853512954b52b81294058a916ab1430c719
KyungMin <2004ckck@naver.com>
2021-04-06 19:39:34 +0900

    [UPDATE] Data Class 변수 변경

--- app/src/main/java/com/cotion/it_consultiong/model/recycler_model/BoardData.kt
+++ app/src/main/java/com/cotion/it_consultiong/model/recycler_model/BoardData.kt
@@ -1,37 +1,19 @@
 package com.cotion.it_consultiong.model.recycler_model
 
 import android.os.Parcelable
-import android.widget.ImageButton
-import com.xwray.groupie.Group
-import com.xwray.groupie.GroupDataObserver
-import com.xwray.groupie.Item
+import android.widget.ImageView
 import kotlinx.android.parcel.Parcelize
 
+@Suppress("DEPRECATED_ANNOTATION")
 @Parcelize
 data class BoardData(
-    val name: String,
-    val title: String,
-    val context: String?,
-    val profile: String,
-    val day: String
-) : Parcelable, Group {
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    //  var class_number: String,
+    val contents: String?=null,
+    //  val grade_class: String?,
+    //  val grade: String?,
+    val job: String?=null,
+    val name: String?=null,
 
-    override fun getItem(position: Int): Item<*> {
-        TODO("Not yet implemented")
-    }
-
-    override fun getPosition(item: Item<*>): Int {
-        TODO("Not yet implemented")
-    }
-
-    override fun registerGroupDataObserver(groupDataObserver: GroupDataObserver) {
-        TODO("Not yet implemented")
-    }
-
-    override fun unregisterGroupDataObserver(groupDataObserver: GroupDataObserver) {
-        TODO("Not yet implemented")
-    }
-}
+    val title: String?=null,
+    val When: String?=null,
+) : Parcelable
