d70066a7f65f38ef8b1187e26170bf8638bde7b6
KyungMin <2004ckck@naver.com>
2021-04-07 14:46:18 +0900

    [UPDATE] When -> day

--- app/src/main/java/com/cotion/it_consultiong/model/recycler_model/BoardData.kt
+++ app/src/main/java/com/cotion/it_consultiong/model/recycler_model/BoardData.kt
@@ -7,13 +7,9 @@ import kotlinx.android.parcel.Parcelize
 @Suppress("DEPRECATED_ANNOTATION")
 @Parcelize
 data class BoardData(
-    //  var class_number: String,
-    val contents: String?=null,
-    //  val grade_class: String?,
-    //  val grade: String?,
-    val job: String?=null,
-    val name: String?=null,
-
-    val title: String?=null,
-    val When: String?=null,
+    val contents: String? = null,
+    val job: String? = null,
+    val name: String? = null,
+    val title: String? = null,
+    val day: String? = null,
 ) : Parcelable
